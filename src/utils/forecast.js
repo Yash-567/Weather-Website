156510c937f8247916901515aedeee5b61d948a8
Yash Sonar <yashsonar213@gmail.com>
2019-06-12 18:37:20 +0530

    added about

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, body.daily.summary + ' It is currently ' + body.currently.temperature + ' degress out. There is a ' + body.currently.precipProbability + '% chance of rain.')
+            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. There is a ' + body.currently.precipProbability + '% chance of rain. The max temperature today would be '+ body.daily.data[0].temperatureMax+' and the minimum would be '+body.daily.data[0].temperatureMin+'. This place is in '+body.timezone+' timezone.')
         }
     })
 }
